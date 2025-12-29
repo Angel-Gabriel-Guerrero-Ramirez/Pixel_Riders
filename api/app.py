9f2a1640491060b07d4d9639b2dc14edcadbb850
Gabo Guerrero <gaboguerrero2024@gmail.com>
2025-12-29 14:46:32 -0600

    API update

--- api/app.py
+++ api/app.py
@@ -159,4 +159,5 @@ def submit_score():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
